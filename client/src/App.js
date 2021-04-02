f5282a90c8d4d49bd396add7485245fd92a52313
TGab99 <55789282+TGab99@users.noreply.github.com>
2021-04-02 09:25:22 +0200

    Update App.js
    I imported the Nav.js file and inserted it in the div. And I expended the one Route tag with exact.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Home from './Home';
+import Nav from './Nav';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Nav/>
         <Switch>
-          <Route path="/" component={Home}/>
+          <Route path="/" exact component={Home}/>
         </Switch>
       </div>
     </Router>
