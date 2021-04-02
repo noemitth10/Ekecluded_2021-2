60ad3e69040048b16838dfa8d4298db83ddc383f
HorvathIstvan9 <71818240+HorvathIstvan9@users.noreply.github.com>
2021-04-02 14:15:49 +0200

    Fixed route path to about page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -10,7 +10,7 @@ function App() {
         <Nav/>
         <Switch>
           <Route path="/" exact component={Home}/>
-          <Route path="/about" component={Home}/>
+          <Route path="/about" component={About}/>
         </Switch>
       </div>
     </Router>
