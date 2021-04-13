b4f9590c449f9bf35fd0d54cc3f6ee7be79c6ba4
Kornel0120 <61868148+Kornel0120@users.noreply.github.com>
2021-04-13 13:33:14 +0200

    Register route was added in App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,6 +2,7 @@ import '../App.css';
 import Home from './Home';
 import About from './About';
 import Nav from '../layout/Nav';
+import Register from './authentication/Register'
 import PageNotFound from './PageNotFound';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import ListBooks from './books/ListBooks';
@@ -18,6 +19,7 @@ function App() {
           <Route path="/about" component={About}/>
           <Route path='/list_books' component={ListBooks}/> 
           <Route exact path="/book-:book_id" component={Book} />
+          <Route path="/Register" exact component={Register}/>
           <Route component={PageNotFound}/>
         </Switch>
       </div>
