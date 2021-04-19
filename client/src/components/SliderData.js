7464f953e786dfb20e536970b36acbf930003694
TGab99 <55789282+TGab99@users.noreply.github.com>
2021-04-19 18:03:13 +0200

    Create SliderData.js
    I created the SliderData.js file.

---
+++ client/src/components/SliderData.js
@@ -0,0 +1,14 @@
+export const sliderData = [
+    {
+        image: 'https://images.unsplash.com/photo-1516979187457-637abb4f9353?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1500&q=80',
+        text: 'Huge Selection Of Books'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1497633762265-9d179a990aa6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1504&q=80',
+        text: 'This Week 20-80% discount'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1512418490979-92798cec1380?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1500&q=80',
+        text: 'Free Shipping Over 10000 FT',
+    }
+];
