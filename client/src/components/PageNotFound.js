b9c9bd20b097cadf94d9e5f06fa26e6dbd136929
Kornel0120 <61868148+Kornel0120@users.noreply.github.com>
2021-04-26 16:08:37 +0200

    Css for the page not found site was created and written.

--- client/src/components/PageNotFound.js
+++ client/src/components/PageNotFound.js
@@ -1,10 +1,12 @@
+import '../PageNotFound.css';
 import React from 'react'
 
 function PageNotFound(){
     return(
         <div>
             <h1>404</h1>
-            <h2>Page Not Found</h2>
+            <h2>Page not found</h2>
+            <p>The page you were looking for doesn't exist. You may have mistyped the addres or the page may have moved. </p>
         </div>
     );
 }
