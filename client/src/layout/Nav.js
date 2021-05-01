ec03bc4ab20122c72ea7b4105628e8109d31155b
TGab99 <55789282+TGab99@users.noreply.github.com>
2021-05-01 11:34:39 +0200

    Update Nav.js
    I updated the Nav.js file. I added a dropdown menu to the menubar.

--- client/src/layout/Nav.js
+++ client/src/layout/Nav.js
@@ -1,6 +1,7 @@
 import '../Nav.css';
 import { Link } from 'react-router-dom';
 import book from '../book-icon.svg';
+import ListCategories from '../components/ListCategories';
 
 function Nav() {
   return (
@@ -10,8 +11,11 @@ function Nav() {
       </Link>
       <ul className="nav-style">
         <Link className="nav-links" to="/books">
-          <li>
+          <li id="dropdown">
             Books
+            <div class="dropdown-content">
+              <ListCategories/>
+            </div>
           </li>
         </Link>
         <Link className="nav-links" to="/about">
