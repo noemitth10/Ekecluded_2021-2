2de624d2f3068301c3c0039fa928300521279dda
TGab99 <55789282+TGab99@users.noreply.github.com>
2021-05-04 15:04:17 +0200

    Update SliderData.js
    I modified the images' text.

--- client/src/components/SliderData.js
+++ client/src/components/SliderData.js
@@ -1,14 +1,14 @@
 export const sliderData = [
     {
         image: 'https://images.unsplash.com/photo-1516979187457-637abb4f9353?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1500&q=80',
-        text: 'Huge Selection Of Books'
+        text: 'Könyvek hatalmas választéka'
     },
     {
         image: 'https://images.unsplash.com/photo-1497633762265-9d179a990aa6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1504&q=80',
-        text: 'This Week 20-80% discount'
+        text: 'Ezen a héten 20-80% kedvezmény'
     },
     {
         image: 'https://images.unsplash.com/photo-1512418490979-92798cec1380?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1500&q=80',
-        text: 'Free Shipping Over 10000 FT',
+        text: '10000 Ft felett ingyenes szállítás',
     }
 ];
