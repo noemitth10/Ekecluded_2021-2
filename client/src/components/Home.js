374be88246ceb75d2b40f1fa9aa97720763bacfe
TGab99 <55789282+TGab99@users.noreply.github.com>
2021-05-17 10:38:19 +0200

    Update Home.js
    I imported the SomeBooks

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,14 @@
 import '../App.css';
 import ImageSlider from './ImageSlider';
 import { sliderData } from './SliderData';
+import SomeBooks from './books/SomeBooks';
 
 function Home() {
   return (
     <>
       <ImageSlider slides={sliderData}/>
+      <hr/>
+      <SomeBooks/>
     </>
   );
 }
